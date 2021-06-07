1d3f918f13eeb05dbf2212cf7fb0b1b71bbb2052
Ryan Ho <hor@flip2.engr.oregonstate.edu>
2021-06-06 22:40:06 -0700

    fixed lint errors

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,14 @@ Test for calc app
 
 import calculator
 
+
 class TestCalculatorApp:
+
     def test_add(self):
-        assert 5 == calculator.add(3,2)
+        assert 5 == calculator.add(3, 2)
 
     def test_sub(self):
-        assert 5 == calculator.sub(8,3)	
+        assert 5 == calculator.sub(8, 3)
 
-	def test_mult(self):
-	    assert 4 == calculator.mult(2,2)
+    def test_mult(self):
+        assert 4 == calculator.mult(2, 2)
